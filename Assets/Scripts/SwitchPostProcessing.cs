3f0b484b3eb7e0dc189656ddc40b68d1ea003c0a
Jason Brennan <brenjaso@gmail.com>
2019-11-14 23:21:38 -0500

    Added play area, water, water effect, player and player controller

---
+++ Assets/Scripts/SwitchPostProcessing.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PostProcessing;
+using UnityStandardAssets.Water;
+
+public class SwitchPostProcessing : MonoBehaviour
+{
+    public PostProcessingProfile normal;
+    public PostProcessingProfile underwater;
+
+    private PostProcessingBehaviour cameraFx;
+    private Water water;
+
+    void Start()
+    {
+        cameraFx = FindObjectOfType<PostProcessingBehaviour>();
+        water = GameObject.Find("WaterSurface").GetComponent<Water>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player")) {
+            cameraFx.profile = underwater;
+            water.waterMode = Water.WaterMode.Refractive;
+        }
+    }
+
+    void OnTriggerExit(Collider other) {
+        if(other.CompareTag("Player")) {
+            cameraFx.profile = normal;
+            water.waterMode = Water.WaterMode.Reflective;
+        }
+    }
+}
